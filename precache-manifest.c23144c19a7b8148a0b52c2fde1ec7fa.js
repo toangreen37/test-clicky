8840fd8c81f359b4881215ba901ecd94a403eb4b
Turbo <toan.takahara@gmail.com>
2019-07-06 17:26:04 -0500

    Updates

---
+++ precache-manifest.c23144c19a7b8148a0b52c2fde1ec7fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "292324e359c16b6b58f7c8bac01c7b15",
+    "url": "/test-clicky/index.html"
+  },
+  {
+    "revision": "fb81bad736410219ad9e",
+    "url": "/test-clicky/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/test-clicky/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "fb81bad736410219ad9e",
+    "url": "/test-clicky/static/js/main.da2ceb92.chunk.js"
+  },
+  {
+    "revision": "67845f7f513beb4d2079",
+    "url": "/test-clicky/static/js/runtime~main.43113bc6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-clicky/static/media/logo.5d5d9eef.svg"
+  }
+]);
